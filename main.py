9f53508ddde2188f5a7ca9ea7589a140ccea193b
Sebas03446 <jsoracipa@unal.edu.co>
2023-09-24 22:41:44 -0500

    feat:look privacy layer

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import scapy.all as scapy
 from scapy.layers.dot11 import Dot11
 from scapy.layers.dot11 import Dot11Beacon
-from scapy.layers.dot11 import Dot11AssoReq
+from scapy.layers.dot11 import Dot11AssoReq, Dot11QoS,Dot11WEP, Dot11WPA, Dot11WPA2
 
 
 def packet_handler(packet):
@@ -18,10 +18,42 @@ def packet_handler(packet):
         dot11_layer = packet[Dot11Beacon]
         print(f"802.11 Beacon Frame Captured: Type={dot11_layer.type}, Subtype={dot11_layer.subtype}")
         print(packet.show())
+        capability_info = packet[Dot11Beacon].capability
+        # 5.2
+        if capability_info.privacy == 1:
+            print("Security: Enabled")
+            # Analizar más a fondo los elementos para determinar WEP, WPA, WPA2, WPA3
+            if packet.haslayer(Dot11WEP):
+                print("Encryption: WEP")
+            elif packet.haslayer(Dot11WPA):
+                print("Encryption: WPA")
+            elif packet.haslayer(Dot11WPA2):
+                print("Encryption: WPA2")
+            else:
+                print("Security: Disabled")
     
     if packet.haslayer(Dot11AssoReq):
         dot11_layer = packet[Dot11AssoReq]
         print(f"802.11 Association Request Frame Captured: Type={dot11_layer.type}, Subtype={dot11_layer.subtype}")
         print(packet.show())
+        # 5.2
+        if capability_info.privacy == 1:
+            print("Security: Enabled")
+            # Analizar más a fondo los elementos para determinar WEP, WPA, WPA2, WPA3
+            if packet.haslayer(Dot11WEP):
+                print("Encryption: WEP")
+            elif packet.haslayer(Dot11WPA):
+                print("Encryption: WPA")
+            elif packet.haslayer(Dot11WPA2):
+                print("Encryption: WPA2")
+            else:
+                print("Security: Disabled")
+    
+    # 5.2
+    if packet.haslayer(Dot11QoS):
+            print("\n802.11 Data Frame with QoS captured:")
+            print(f"Priority: {packet[Dot11QoS].TID}")
+    
+    
 
 scapy.sniff(iface="wlo1", prn=packet_handler, store=0)
